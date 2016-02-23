fb8c61528eb2660658cc43462bf5c177f5dd99c4
sablancom <sablancom@unal.edu.co>
2016-02-23 18:46:13 -0500

    This is a awesome danm english message boys!!!!

---
+++ src/groovy/HelloWorld.groovy
@@ -0,0 +1,12 @@
+/**
+ * Created by Andres on 23/02/2016.
+ */
+class HelloWorld {
+
+    def world = {
+
+        final def var = render("Hello World!")
+        var
+    }
+    /*def index = { } */
+}
